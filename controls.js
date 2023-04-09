2522bd2958b20356f27ba34842a1b1ca16c92513
benjaminliang2 <benjaminliang2@gmail.com>
2023-04-08 21:31:37 -1000

    define road, sensor classes and implement with car

--- controls.js
+++ controls.js
@@ -1,45 +1,45 @@
-class Controls { 
+class Controls{
     constructor(){
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.backward = false;
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
         this.#addKeyboardListeners();
-
     }
+
     #addKeyboardListeners(){
-        document.onkeydown=(event) => {
+        document.onkeydown=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = true;
+                    this.left=true;
                     break;
                 case "ArrowRight":
-                    this.right = true;
+                    this.right=true;
                     break;
-                case "ArrowUp": 
-                    this.forward = true;
+                case "ArrowUp":
+                    this.forward=true;
                     break;
                 case "ArrowDown":
-                    this.backward = true;
+                    this.reverse=true;
                     break;
             }
         }
-        document.onkeyup=(event) => {
+        document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = false;
+                    this.left=false;
                     break;
                 case "ArrowRight":
-                    this.right = false;
+                    this.right=false;
                     break;
-                case "ArrowUp": 
-                    this.forward = false;
+                case "ArrowUp":
+                    this.forward=false;
                     break;
                 case "ArrowDown":
-                    this.backward = false;
+                    this.reverse=false;
                     break;
             }
         }
     }
-
 }
